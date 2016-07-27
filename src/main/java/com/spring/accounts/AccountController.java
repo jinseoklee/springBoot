5bc60a336b6dad395d47e2797167dd5d43b83e52
David <godluv81@gmail.com>
2016-07-27 09:21:02 +0900

    first-1

---
+++ src/main/java/com/spring/accounts/AccountController.java
@@ -0,0 +1,49 @@
+package com.spring.accounts;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+/**
+ * Created by David on 2016-07-26.
+ */
+//@ResponseBody
+//@Controller
+@RestController
+public class AccountController {
+
+    @Autowired
+    private AccountService service;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello Spring Boot 한글 테스트";
+    }
+
+
+
+    @RequestMapping(value = "/accounts", method = RequestMethod.POST)
+    public ResponseEntity createAccount(@RequestBody @Valid AccountDto.Create create,
+                                                 BindingResult result){
+        if(result.hasErrors()){
+            //TODO 에러 응답 본문 추가하기
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+
+        Account newAccount = service.createAccout(create);
+
+        return new ResponseEntity<>(modelMapper.map(newAccount, AccountDto.Response.class), HttpStatus.CREATED);
+    }
+
+
+
+}
